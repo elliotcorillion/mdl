55499d9235bf62518aec8264da626a5c2a041d55
elliot.corillion <elliotcorillion@gmail.com>
2015-10-15 10:25:27 +0200

    Premier commit - ajout dossier modele - ajout dossier services - ajout dossier vue - ajout dossier modele.dao

---
+++ src/gsb/vue/JIFMedecinListeDic.java
@@ -0,0 +1,16 @@
+package gsb.vue;
+
+import javax.swing.JInternalFrame;
+
+public class JIFMedecinListeDic extends JInternalFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public JIFMedecinListeDic(MenuPrincipal menuPrincipal) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
